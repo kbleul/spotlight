b60a678c4a1fe5a8feae3a3775f09fad86933c77
kbleul <kbleulseged@gmail.com>
2024-03-30 12:02:51 +0300

    DONE: services horizontal scroll slowed down fix

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 
@@ -37,200 +37,193 @@ const CONTENT = [
 ];
 
 const Services = () => {
-  const [currentContent, setCurrentContent] = useState(0);
-  const [ref, inView] = useInView({ threshold: 0.9 });
+  const { ref, inView } = useInView({
+    threshold: 0.9,
+  });
 
-  useEffect(() => {
+  const [scrollPrecentage, setScrollPercentage] = useState(0);
+  const [scrollPrecentageUp, setScrollPercentageUp] = useState(0);
+
+  function handleWheel(event: WheelEvent) {
     if (inView) {
       const rootElement = document.getElementById("root-body");
       if (rootElement) {
         rootElement.style.overflowY = "hidden";
       }
+      const container: HTMLElement | null =
+        document.querySelector("#container");
+      const container2: HTMLElement | null =
+        document.querySelector("#container2");
+      const container3: HTMLElement | null =
+        document.querySelector("#container3");
+      const container4: HTMLElement | null =
+        document.querySelector("#container4");
+
+      if (event.deltaY < 0) {
+        setScrollPercentage(0);
+        if (container && container2 && container3 && container4) {
+          container.style.transform = `translateX(+${scrollPrecentageUp}%)`;
+          container2.style.transform = `translateX(+${scrollPrecentageUp}%)`;
+          container3.style.transform = `translateX(+${scrollPrecentageUp}%)`;
+          container4.style.transform = `translateX(+${scrollPrecentageUp}%)`;
+        }
+
+        setScrollPercentageUp((prev) => {
+          if (prev <= 3) {
+            return 0;
+          }
+
+          return Math.max(prev - 5, 5);
+        });
+      } else {
+        setScrollPercentageUp(0);
+        if (container && container2 && container3 && container4) {
+          container.style.transform = `translateX(-${scrollPrecentage}%)`;
+          container2.style.transform = `translateX(-${scrollPrecentage}%)`;
+          container3.style.transform = `translateX(-${scrollPrecentage}%)`;
+          container4.style.transform = `translateX(-${scrollPrecentage}%)`;
+        }
+
+        setScrollPercentage((prev) => {
+          if (prev >= 250) {
+            const rootElement = document.getElementById("root-body");
+
+            if (rootElement) {
+              rootElement.style.overflowY = "scroll";
+            }
+            return 240;
+          }
+
+          return Math.max(prev + 5, 5);
+        });
+      }
     } else {
       const rootElement = document.getElementById("root-body");
       if (rootElement) {
-        if (rootElement.style.overflowY === "hidden") {
-          rootElement.style.overflowY = "scroll";
-        }
+        rootElement.style.overflowY = "scroll";
       }
+    }
+  }
+
+  useEffect(() => {
+    const rootElement = document.getElementById("root-body");
+
+    console.log(inView);
+    if (inView && rootElement) {
+      console.log("inviewCheck ", inView);
+
+      rootElement.style.overflowY = "hidden";
     }
   }, [inView]);
 
-  useEffect(() => {
-    const handleScroll = (event: any) => {
-      event.preventDefault();
-
-      if (inView) {
-        const delta = event.deltaY;
-
-        if (delta < 0 && currentContent > 0) {
-          setCurrentContent((prevContent) => prevContent - 1);
-        } else if (delta > 0) {
-          setCurrentContent((prevContent) => prevContent + 1);
-        }
-
-        if (currentContent >= 9) {
-          const rootElement = document.getElementById("root-body");
-
-          if (rootElement && rootElement.style.overflowY === "hidden") {
-            rootElement.style.overflowY = "scroll";
-            setTimeout(() => setCurrentContent(0), 2000);
-          }
-        }
-      }
-    };
-
-    window.addEventListener("wheel", handleScroll);
-
-    return () => {
-      window.removeEventListener("wheel", handleScroll);
-    };
-  }, [currentContent, inView]);
-
   return (
     <article
+      id="container-article"
       ref={ref}
-      className={
-        currentContent % 2 === 0 && currentContent !== 0
-          ? "relative h-[108vh] bg-white mb-20 "
-          : "relative h-[108vh] bg-black mb-20 "
-      }
+      className="h-[105vh] w-full bg-transparent flex overflow-hidden"
+      onWheel={handleWheel}
     >
-      {inView && (
-        <>
-          {currentContent < 3 && (
-            <div
-              className="h-[108vh] w-full bg-black flex justify-evenly items-center "
-              // initial={inView ? { x: 2000 } : { x: 0 }}
-              // animate={inView ? { x: 0 } : { x: 2000 }}
-              // transition={{ duration: 1 }}
-            >
-              <section className="w-[45%] max-w-[600px] text-white">
-                <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
-                  Services
-                </h2>
+      <div
+        id="container"
+        className="w-full flex-shrink-0 h-[105vh] flex justify-evenly items-center bg-black"
+      >
+        <section className="w-[45%] max-w-[600px] text-white">
+          <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
+            Services
+          </h2>
 
-                <div className="font-bold text-2xl">
-                  <h4>{CONTENT[0].title}</h4>
-                </div>
+          <div className="font-bold text-2xl">
+            <h4>{CONTENT[0].title}</h4>
+          </div>
 
-                <div className="max-w-[600px] mt-4">{CONTENT[0].body}</div>
-              </section>
-              <section className="w-[45%] flex justify-center items-start  pt-4 ">
-                <motion.img
-                  src={CONTENT[0].img}
-                  alt="logo"
-                  className="max-w-[25rem] max-h-[25rem]"
-                  initial={{ x: 1000 }}
-                  animate={{ x: 0 }}
-                  transition={{ duration: 0.7 }}
-                />
-              </section>
-            </div>
-          )}
+          <div className="max-w-[600px] mt-4">{CONTENT[0].body}</div>
+        </section>
+        <section className="w-[45%] flex justify-center items-start  pt-4 ">
+          <img
+            src={CONTENT[0].img}
+            alt="logo"
+            className="max-w-[25rem] max-h-[25rem]"
+          />
+        </section>
+      </div>
+      <div
+        id="container2"
+        className="w-full flex-shrink-0 h-[105vh] flex justify-evenly items-center bg-[#fce8bb]"
+      >
+        <section className="w-[45%] max-w-[600px]">
+          <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
+            Services
+          </h2>
 
-          {currentContent >= 3 && currentContent < 6 && (
-            <motion.div
-              className="h-[108vh] w-full bg-white flex justify-evenly items-center "
-              initial={inView ? { x: 2000 } : { x: 0 }}
-              animate={inView ? { x: 0 } : { x: 2000 }}
-              transition={{ duration: 1 }}
-            >
-              <section className="w-[45%] max-w-[600px]">
-                <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
-                  Services
-                </h2>
+          <motion.div
+            className="font-bold text-2xl"
+            initial={{ y: -15 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <h4>{CONTENT[1].title}</h4>
+          </motion.div>
 
-                <motion.div
-                  className="font-bold text-2xl"
-                  initial={{ y: -15 }}
-                  animate={{ y: 0 }}
-                  transition={{ duration: 0.8 }}
-                >
-                  <h4>{CONTENT[1].title}</h4>
-                </motion.div>
+          <motion.div
+            className="max-w-[600px] mt-4"
+            initial={{ y: 50 }}
+            animate={{ y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            {CONTENT[1].body}
+          </motion.div>
+        </section>
+        <section className="w-[45%] flex justify-center items-start  pt-4 ">
+          <img
+            src={CONTENT[1].img}
+            alt="logo"
+            className="max-w-[25rem] max-h-[25rem]"
+          />
+        </section>
+      </div>
+      <div
+        id="container3"
+        className="w-full flex-shrink-0 h-[105vh] flex justify-evenly items-center bg-black"
+      >
+        <section className="w-[45%] max-w-[600px] text-white">
+          <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
+            Services
+          </h2>
 
-                <motion.div
-                  className="max-w-[600px] mt-4"
-                  initial={{ y: 50 }}
-                  animate={{ y: 0 }}
-                  transition={{ duration: 0.8 }}
-                >
-                  {CONTENT[1].body}
-                </motion.div>
-              </section>
-              <section className="w-[45%] flex justify-center items-start  pt-4 ">
-                <img
-                  src={CONTENT[1].img}
-                  alt="logo"
-                  className="max-w-[25rem] max-h-[25rem]"
-                />
-              </section>
-            </motion.div>
-          )}
+          <div className="font-bold text-2xl">
+            <h4>{CONTENT[2].title}</h4>
+          </div>
 
-          {currentContent >= 6 && currentContent < 9 && (
-            <motion.div
-              className="h-[108vh] w-full bg-black flex justify-evenly items-center "
-              initial={inView ? { x: 2000 } : { x: 0 }}
-              animate={inView ? { x: 0 } : { x: 2000 }}
-              transition={{ duration: 1 }}
-            >
-              <section className="w-[45%] max-w-[600px] text-white">
-                <h2 className="text-[115px] font-extrabold text-[#4F4F4F] mb-6">
-                  Services
-                </h2>
+          <div className="max-w-[600px] mt-4">{CONTENT[2].body}</div>
+        </section>
+        <section className="w-[45%] flex justify-center items-start  pt-4 ">
+          <img
+            src={CONTENT[2].img}
+            alt="logo"
+            className="max-w-[25rem] max-h-[25rem]"
+          />
+        </section>
+      </div>
+      <div
+        id="container4"
+        className="w-full flex-shrink-0 h-[105vh] flex flex-col justify-evenly items-center bg-white"
+      >
+        <img className="" src={CONTENT[3].img} alt="" />
 
-                <div className="font-bold text-2xl">
-                  <h4>{CONTENT[2].title}</h4>
-                </div>
-
-                <div className="max-w-[600px] mt-4">{CONTENT[2].body}</div>
-              </section>
-              <section className="w-[45%] flex justify-center items-start  pt-4 ">
-                <img
-                  src={CONTENT[2].img}
-                  alt="logo"
-                  className="max-w-[25rem] max-h-[25rem]"
-                />
-              </section>
-            </motion.div>
-          )}
-
-          {currentContent >= 9 && (
-            <article className="h-[108vh]">
-              <section className="w-1/2 "></section>
-
-              <motion.div
-                className="absolute top-0 right-0 w-2/5 h-[110vh] bg-white flex flex-col justify-start items-center px-[5%]  pt-[30vh] "
-                initial={inView ? { x: 2000 } : { x: 0 }}
-                animate={inView ? { x: 0 } : { x: 2000 }}
-                transition={{ duration: 1 }}
-              >
-                <img className="" src={CONTENT[3].img} alt="" />
-
-                <p className="py-16 text-[#777777] font-medium">
-                  Lorem ipsum dolor sit amet consectetur. Ut et vestibulum
-                  lectus nullam tellus aliquet pellentesque a dui. Nunc leo at
-                  sit fusce.
-                </p>
-                <div className="expandButtonContainerThird">
-                  <button
-                    type="button"
-                    className="expandButton bg-white px-4 text-black py-2 border border-black font-normal flex gap-4 items-center"
-                  >
-                    <p className="text-base text-nowrap">{CONTENT[3].body}</p>
-                    <IoIosArrowRoundForward
-                      className="expandButtonIcon"
-                      size={33}
-                    />
-                  </button>
-                </div>
-              </motion.div>
-            </article>
-          )}
-        </>
-      )}
+        <p className="py-16 text-[#777777] font-medium">
+          Lorem ipsum dolor sit amet consectetur. Ut et vestibulum lectus nullam
+          tellus aliquet pellentesque a dui. Nunc leo at sit fusce.
+        </p>
+        <div className="expandButtonContainerThird">
+          <button
+            type="button"
+            className="expandButton bg-white px-4 text-black py-2 border border-black font-normal flex gap-4 items-center"
+          >
+            <p className="text-base text-nowrap">{CONTENT[3].body}</p>
+            <IoIosArrowRoundForward className="expandButtonIcon" size={33} />
+          </button>
+        </div>
+      </div>
     </article>
   );
 };
